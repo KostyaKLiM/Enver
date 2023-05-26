01628d264cdc567b4aa7df0676dd5603583da51c
Klimbutsky Kostya <pravednik2008@ukr.net>
2023-05-26 19:08:26 +0300

    add files

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function navToggle(){
 	var links = document.getElementById('navUL');
 	links.classList.toggle("_active");
 }
+
+$('.owl-carousel').owlCarousel({
+	items:3,
+	lazyLoad:true,
+	loop:true,
+	margin:24,
+	responsive:{
+		0:{
+			 items:1,
+			 nav:true
+		},
+		592:{
+			 items:3,
+			 nav:false
+		},
+		1000:{
+			 items:5,
+			 nav:true,
+			 loop:false
+		}
+  }
+});
